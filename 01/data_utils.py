7b28d3799363f790e8d2a5ed4b13c596df25da22
rdverse <deveshreddy27@gmail.com>
2023-12-14 15:53:18 -0600

    clean up

--- 01/data_utils.py
+++ 01/data_utils.py
@@ -6,14 +6,20 @@ import os
 import time
 import logging
 
-
 class TextProcessor:
     def __init__(self, file_path, n):
         self.file_path = file_path
         self.n = n
 
     def process_text(self)->list:
-        
+        """ 
+        Main function to process text.
+         Args:
+                file_path (str): Path to the text file.
+                n (int): n-gram
+        Returns:
+                list: List of cleaned tokens.
+        """ 
         text = self.read_text_file()
         functions = [self.split_text_by_sentence,
                      self.replace_em_dashes, 
@@ -30,6 +36,13 @@ class TextProcessor:
         return text
 
     def read_text_file(self)->str:
+        """
+        Read text file.
+        Args:
+            file_path (str): Path to the text file.
+        Returns:
+            str: Text read from the file.
+        """
         start_time = time.time()
         logging.debug(f"Reading file {self.file_path}")
         with open(self.file_path, 'r', encoding='utf-8') as file:
@@ -41,15 +54,10 @@ class TextProcessor:
     def split_text_by_sentence(self, text)->list:
         
         text = text.replace("\n", "")
-
         text = re.sub(r'\s+', ' ', text)  
         sentence_endings = r'[.?:]'
         # Split the text into sentences and add sentence markers
         sentences = re.split(sentence_endings, text)
-        # trim sentences - whitespace    
-        #sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
-        #print(sentences)
-        #print(sentences)
         logging.info(f"Split text into {len(sentences)} sentences")
         return sentences
     
@@ -114,141 +122,3 @@ class TextProcessor:
         sentences = [[word for word in sentence if word] for sentence in sentences if any(sentence)]
         sentences = [sentence for sentence in sentences if len(sentence)>3]
         return sentences
-    
-    
-
-# class TextProcessor:
-#     def __init__(self, text, n):
-#         self.text = text
-#         self.n = n
-#         self.vocab = self.get_vocab(self.text)
-#         self.ngrams = self._build_ngrams()
-
-# # Function to read the UTF-8 text from a file
-# def read_text_file(file_path):
-#     """
-#     Read UTF-8 text from a file.
-
-#     Args:
-#         file_path (str): Path to the text file.
-
-#     Returns:
-#         str: The text content read from the file.
-#     """
-#     start_time = time.time()
-#     logging.debug(f"Reading file {file_path}")
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         text = file.read()
-
-#     logging.info(f"Completed reading file in {time.time() - start_time} seconds")
-#     return text
-
-# def split_text_by_sentence(text):
-    
-#     text = text.replace("\n", "")
-
-#     text = re.sub(r'\s+', ' ', text)  
-#     sentence_endings = r'[.?:]'
-#     # Split the text into sentences and add sentence markers
-#     sentences = re.split(sentence_endings, text)
-#     # trim sentences - whitespace    
-#     #sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
-#     #print(sentences)
-#     #print(sentences)
-#     logging.info(f"Split text into {len(sentences)} sentences")
-#     return sentences
-
-# # Function to add sentence markers to the text
-# def add_sentence_markers(sentences, n):
-#     """
-#     Add sentence markers <s> and </s> at the beginning and end of each sentence.
-
-#     Args:
-#         text (str): Input text
-#         n (int) : n-gram
-#     Returns:
-#         str: Text with sentence markers added.
-#     """
-#     return [['<s>']*(n-1) + sentence + ['</s>']*(n-1) for sentence in sentences]
-
- 
-
-# # Function to replace em-dashes with spaces
-# def replace_em_dashes(sentences):
-#     """
-#     Replace em-dashes with spaces.
-
-#     Args:
-#         text (str): Input text.
-
-#     Returns:
-#         str: Text with em-dashes replaced wit spaces.
-#     """
-#     # Replace em-dashes with spaces
-#     cleaned_text = [sentence.replace('—', ' ') for sentence in sentences]
-#     return cleaned_text
-
-# # Function to convert text to lowe—'r case
-# def convert_to_lower_case(sentences):
-#     """
-#     Convert text to lower case.
-
-#     Args:
-#         text (str): Input text.
-
-#     Returns:
-#         str: Text in lower case.
-#     """
-#     # Convert the text to lower case
-#     lower_case_text = [sentence.lower() for sentence in sentences]
-#     return lower_case_text
-
-# # Function to tokenize and remove special characters
-# def tokenize_and_remove_special_characters(sentences):
-#     """
-#     Tokenize text and remove special characters (except hyphens and apostrophes between letters).
-
-#     Args:
-#         text (str): Input text.
-
-#     Returns:
-#         list: List of cleaned tokens.
-#     """
-#     # Define a regular expression pattern to split on whitespace and remove special characters
-#     token_pattern = r'[^A-Za-z0-9\'-]+'
-
-#     # Tokenize the text while preserving hyphens and apostrophes
-#     tokens = [re.split(token_pattern, sentence) for sentence in sentences]
-#     return tokens 
-
-# def remove_empty_strings(sentences):
-#     sentences = [[word for word in sentence if word] for sentence in sentences if any(sentence)]
-#     sentences = [sentence for sentence in sentences if len(sentence)>3]
-#     return sentences
-
-# # Main function to perform data cleanup
-# def data_cleanup(file_path, n):
-#     """
-#     Perform data cleanup on the text from the specified file.
-
-#     Args:
-#         file_path (str): Path to the text file.
-
-#     Returns:
-#         list: List of cleaned tokens.
-#     """
-#     text = read_text_file(file_path)
-#     functions = [split_text_by_sentence,
-#                  replace_em_dashes, 
-#                  convert_to_lower_case,
-#                  tokenize_and_remove_special_characters,
-#                  add_sentence_markers, 
-#                  remove_empty_strings]
-    
-#     for function in functions:
-#         start_time = time.time()
-#         logging.debug(f"Running function {function.__name__}")
-#         text = function(text)
-#         logging.info(f"Completed running function {function.__name__} in {time.time() - start_time} seconds")
-        
-#     return text#final_tokens#,#cleaned_tokens
