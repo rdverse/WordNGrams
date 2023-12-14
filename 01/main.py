d43fd3d8ca9542cd25c8dba4896cac94a011f711
rdverse <deveshreddy27@gmail.com>
2023-12-14 12:10:12 -0600

    change running mode

--- 01/main.py
+++ 01/main.py
@@ -3,11 +3,12 @@ from ngrams import *
 
 if __name__=="__main__":
     file_path = 'pg74.txt'  # Replace with the path to your text file
-    cleaned_tokens = data_cleanup(file_path)
+    n=2
+    cleaned_tokens = data_cleanup(file_path, n)
     #print(cleaned_tokens)
     corpus = []
     for tokens in cleaned_tokens:
         corpus.extend(tokens)    
-    n=1
-    generator = NGramTextGenerator(file_path,cleaned_tokens, n)
+    generator = NGramTextGenerator(cleaned_tokens, n)
     generated_text = generator.generate_sentence()
+    print(generated_text)
